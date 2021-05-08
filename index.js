66dfb8f6325297645d223ab5c40bf840ae0e36a4
Pratap Tyagi <tyagipratap111@gmail.com>
2021-05-08 16:21:24 +0530

    Added posting any data on database feature

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import connectDb from "./config/db.js";
+import getUrl from "./routes/index.js";
+import createUrl from "./routes/url.js";
+
 const app = express();
 
 // DB configuration
 connectDb();
 
 app.use(express.json({ extended: false }));
+app.use("/", createUrl);
+app.use("/api/url", getUrl);
 
 const PORT = process.env.PORT || 8080;
 
---
+++ routes/url.js
@@ -0,0 +1,45 @@
+import express from "express";
+import validUrl from "valid-url";
+import shortid from "shortid";
+import config from "config";
+import model from "../models/dbModel.js";
+
+const router = express.Router();
+
+router.post("/shorten", async (req, res) => {
+  const { longUrl } = req.body;
+  const baseUrl = config.get("baseUrl");
+
+  if (!validUrl.isUri(baseUrl)) {
+    res.status(401).json("Invalid Base Url");
+  }
+
+  const urlCode = shortid.generate();
+
+  if (validUrl.isUri(longUrl)) {
+    try {
+      const url = await model.findOne({ longUrl });
+      if (url) {
+        res.json(url);
+      } else {
+        const shortUrl = baseUrl + "/" + urlCode;
+        url = new model({
+          longUrl,
+          shortUrl,
+          urlCode,
+          date: new Date(),
+        });
+        await url.save();
+
+        res.json(url);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json("Server Error");
+    }
+  } else {
+    res.status(401).json("Invalid Long Url");
+  }
+});
+
+export default router;
